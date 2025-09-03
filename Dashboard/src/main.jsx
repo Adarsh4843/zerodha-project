ff5ba3453724944ba0952b9f9da27fb8e2d8df5a
aadarsh gadhavi <aadarshgadhavi4843@gmail.com>
2025-09-03 15:48:34 +0530

    update dashboard

--- Dashboard/src/main.jsx
+++ Dashboard/src/main.jsx
@@ -6,13 +6,12 @@ import "./index.css";
 
 // Use the environment variable from Render
 const API_URL = import.meta.env.VITE_API_URL;
-
 export { API_URL }; // export so components can import it
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   <BrowserRouter basename="/dashboard">
+   <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
       </Routes>
